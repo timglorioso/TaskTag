80e2a69f6eb781b4867f1019a6ab98bbe8d9397a
timglorioso <glorioso.tim@gmail.com>
2016-04-18 13:57:45 -0400

    add sample data

--- TaskTag/ListViewController.swift
+++ TaskTag/ListViewController.swift
@@ -10,12 +10,50 @@ import UIKit
 
 class ListViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+   var tasks = [Task]()
+   var tags = [Tag]()
+
+   override func viewDidLoad() {
+      createSampleData()
+   }
+
    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-      return 0
+      return tasks.count
    }
 
    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-      return UITableViewCell()
+
+      let task = tasks[indexPath.row]
+      var cell: UITableViewCell
+
+      if task.tags.isEmpty {
+         cell = tableView.dequeueReusableCellWithIdentifier("NoTagsCell", forIndexPath: indexPath)
+      } else {
+         cell = tableView.dequeueReusableCellWithIdentifier("TagsCell", forIndexPath: indexPath)
+
+         var tagsLabel = ""
+         for tag in task.tags {
+            tagsLabel.appendContentsOf("#" + tag.name + " ")
+         }
+         cell.detailTextLabel?.text = tagsLabel
+      }
+
+      cell.textLabel?.text = task.name
+
+      return cell
+   }
+
+   func createSampleData() {
+
+      let professionalTag = Tag("professional")
+      let schoolTag = Tag("school")
+      let leisureTag = Tag("leisure")
+      tags += [professionalTag, schoolTag, leisureTag]
+
+      let finishThisAppTask = Task("finish this app", withTags: [professionalTag, schoolTag])
+      let finishFontTask = Task("finish font", withTags: [leisureTag])
+      let eatPizzaTask = Task("eat pizza", withTags: nil)
+      tasks += [finishThisAppTask, finishFontTask, eatPizzaTask]
    }
 }
 
